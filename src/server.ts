2b25cce06d7594390141ffe5caad1daf8569ac4d
Matheus Ramalho <matheusramalho@hotmail.com.br>
2021-12-22 11:05:24 -0300

    Debug config and route for create category of a car, with object assign for populate class category.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import express from "express";
+import { categoriesRoutes } from "./routes/categories.routes";
 
+// Initialize API and set configurations
 const app = express();
+app.use(express.json());
 
+// ROUTES
+app.use("/categories", categoriesRoutes);
+
+// Listen Port
 app.listen(3333, () => console.log("Server is running!"));
